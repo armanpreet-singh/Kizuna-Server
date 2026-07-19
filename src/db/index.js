7a6ce06b88449bb064093722d1a1ffcbb46f7324
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-19 17:41:19 +0530

    feat: add MongoDB connection logic and update ESLint configuration

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { config } from "../config/config.js";
+import { DB_NAME } from "../constants/constants.js";
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(`${config.mongoUri}/${DB_NAME}`);
+
+    console.log(
+      `✅ MongoDB connected successfully: ${connection.connection.host}`,
+    );
+
+    return connection;
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
