2fe127e12a320ca56137e54046270662d8897e56
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-19 18:11:28 +0530

    feat: add rate limiting and HPP middleware for enhanced security in app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,28 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import hpp from "hpp";
+import rateLimit from "express-rate-limit";
+
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100,
+  message: {
+    success: false,
+    message: "😒  Too many requests. Please try again later.",
+  },
+});
+
 app.use(helmet());
+
 app.use(morgan("dev"));
+
+app.use(hpp());
+
+app.use(limiter);
 
 // Middlewares
 app.use(
