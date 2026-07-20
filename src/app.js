9fc6cbe77e531348b7b2ac683ee43a607bdded8b
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-20 22:14:52 +0530

    feat: add authentication routes to the app

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import hpp from "hpp";
 import rateLimit from "express-rate-limit";
+import authRouter from "./routes/auth.routes.js";
 
 import errorHandler from "./middleware/error.middleware.js";
 
@@ -40,7 +41,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
-
+app.use("/api/v1/auth", authRouter);
 // Health Check Route
 app.get("/", (req, res) => {
   res.status(200).json({
