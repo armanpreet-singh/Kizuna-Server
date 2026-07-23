dc684d6dc8e066e67d265f8f84e1532732dfaacc
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-07-23 15:23:30 +0530

    feat: add conversation constants for handling conversation types and participant limits

---
+++ src/constants/conversation.constants.js
@@ -0,0 +1,12 @@
+export const CONVERSATION_TYPES = Object.freeze({
+  DIRECT: "direct",
+  GROUP: "group",
+});
+
+export const PARTICIPANT_LIMITS = Object.freeze({
+  DIRECT: 2,
+  GROUP_MIN: 2,
+});
+
+export const DEFAULT_GROUP_AVATAR =
+  "https://res.cloudinary.com/demo/image/upload/default-group-avatar.png";
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export * from "./conversation.constants.js";
+export * from "./message.constants.js";
+export * from "./notification.constants.js";
+export * from "./socket.constants.js";
