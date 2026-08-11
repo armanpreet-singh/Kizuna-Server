e9583b0d705ab0cf0ac48c8929c8c652414e02c2
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-08-11 22:08:49 +0530

    feat: freeze message types and limits constants for immutability

--- src/constants/message.constants.js
+++ src/constants/message.constants.js
@@ -1,9 +1,9 @@
-export const MESSAGE_TYPES = {
+export const MESSAGE_TYPES = Object.freeze({
   TEXT: "text",
   IMAGE: "image",
   FILE: "file",
-};
+});
 
-export const MESSAGE_LIMITS = {
+export const MESSAGE_LIMITS = Object.freeze({
   CONTENT_MAX: 5000,
-};
+});
