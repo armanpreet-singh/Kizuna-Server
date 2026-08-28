b58eedaeea86c437db78675a405797c0481e3f1d
Armanpreet Singh <singharmanpreet385@gmail.com>
2026-08-28 10:54:40 +0530

    feat: add edit message route to message routes

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -3,6 +3,7 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 import {
   createMessageController,
   getMessagesController,
+  editMessageController,
 } from "../controllers/message.controller.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
@@ -13,5 +14,7 @@ router.use(verifyJWT);
 router.post("/", asyncHandler(createMessageController));
 
 router.get("/:conversationId", asyncHandler(getMessagesController));
+
+router.patch("/:messageId", asyncHandler(editMessageController));
 
 export default router;
